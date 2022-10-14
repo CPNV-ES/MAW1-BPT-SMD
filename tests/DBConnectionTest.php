56ee0524ff10374cac87385687630b29b83aec32
Michaud Samuel <samuel.michaud2000@gmail.com>
2022-10-14 08:50:39 +0200

    DBConnectionTest: Test done

--- tests/DBConnectionTest.php
+++ tests/DBConnectionTest.php
@@ -18,28 +18,82 @@ class DBConnectionTest extends TestCase
 
     public function test_getInstance()
     {
+        //given
+        //refer to setUp
+
+        //when
+        //event is called directly by the assertion
+
+        //then
         self::assertSame(self::$dbConnection, DBConnection::getInstance(DB_DNS, DB_USER, DB_PASSWORD));
     }
 
     public function test_getPDO()
     {
+        //given
+        //refer to setUp
+
+        //when
+        //event is called directly by the assertion
+
+        //then
         self::assertSame(PDO::class, get_class(self::$dbConnection->getPDO()));
     }
 
-    public function test_execute()
+    public function test_execute_select()
     {
+        //given
+        //refer to setUp
+
+        //when
+        //event is called directly by the assertion
+
+        //then
         self::assertIsArray(self::$dbConnection->execute("SELECT * FROM exercises;", Exercise::class));
+    }
+
+    public function test_execute_insert()
+    {
+        //given
+        //refer to setUp
+
+        //when
+        //event is called directly by the assertion
+
+        //then
         self::assertIsBool(
             self::$dbConnection->execute("INSERT INTO exercises (title) VALUES (:title);", Exercise::class, [
                 'title' => 'test DBConnection 1'
             ])
         );
+    }
+
+    public function test_execute_update()
+    {
+        //given
+        //refer to setUp
+
+        //when
+        //event is called directly by the assertion
+
+        //then
         self::assertIsBool(
             self::$dbConnection->execute("UPDATE exercises SET title = :title1 WHERE title = :title2;", Exercise::class, [
                 'title1' => 'test DBConnection 2',
                 'title2' => 'test DBConnection 1'
             ])
         );
+    }
+
+    public function test_execute_delete()
+    {
+        //given
+        //refer to setUp
+
+        //when
+        //event is called directly by the assertion
+
+        //then
         self::assertIsBool(
             self::$dbConnection->execute("DELETE FROM exercises WHERE title = :title;", Exercise::class, [
                 'title' => 'test DBConnection 2'
@@ -49,6 +103,13 @@ class DBConnectionTest extends TestCase
 
     public function test_getLastItemId()
     {
+        //given
+        //refer to setUp
+
+        //when
+        //event is called directly by the assertion
+
+        //then
         self::assertIsInt(self::$dbConnection->getLastItemId());
     }
 }
