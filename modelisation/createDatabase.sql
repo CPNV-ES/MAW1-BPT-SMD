e10cc51bc0bd8d00f2b8795ec21974fff54c8654
Benoit <benoit.pierrehumbert.pro@gmail.com>
2022-10-31 11:03:00 +0100

    createDatabase.sql: Add drop table if exists

--- modelisation/createDatabase.sql
+++ modelisation/createDatabase.sql
@@ -1,13 +1,15 @@
 -- --------------------------------------------------------
 -- Hôte:                         127.0.0.1
--- Version du serveur:           10.10.1-MariaDB - mariadb.org binary distribution
+-- Version du serveur:           10.9.1-MariaDB - mariadb.org binary distribution
 -- SE du serveur:                Win64
--- HeidiSQL Version:             11.3.0.6295
+-- HeidiSQL Version:             12.1.0.6537
 -- --------------------------------------------------------
 
 /*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
 /*!40101 SET NAMES utf8 */;
 /*!50503 SET NAMES utf8mb4 */;
+/*!40103 SET @OLD_TIME_ZONE=@@TIME_ZONE */;
+/*!40103 SET TIME_ZONE='+00:00' */;
 /*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
 /*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
 /*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
@@ -18,7 +20,8 @@ DROP DATABASE IF EXISTS `looper`;
 CREATE DATABASE IF NOT EXISTS `looper` /*!40100 DEFAULT CHARACTER SET utf8mb3 */;
 USE `looper`;
 
--- Listage de la structure de la table looper. exercises
+-- Listage de la structure de table looper. exercises
+DROP TABLE IF EXISTS `exercises`;
 CREATE TABLE IF NOT EXISTS `exercises` (
   `id` int(11) NOT NULL AUTO_INCREMENT,
   `title` varchar(45) NOT NULL,
@@ -29,20 +32,22 @@ CREATE TABLE IF NOT EXISTS `exercises` (
 
 -- Les données exportées n'étaient pas sélectionnées.
 
--- Listage de la structure de la table looper. fields
+-- Listage de la structure de table looper. fields
+DROP TABLE IF EXISTS `fields`;
 CREATE TABLE IF NOT EXISTS `fields` (
   `id` int(11) NOT NULL AUTO_INCREMENT,
   `label` varchar(45) NOT NULL,
   `value_kind` varchar(45) NOT NULL,
   `exercises_id` int(11) NOT NULL,
   PRIMARY KEY (`id`),
-  UNIQUE KEY `label_UNIQUE` (`label`,`exercises_id`) USING BTREE,
+  UNIQUE KEY `label` (`label`,`exercises_id`) USING BTREE,
   KEY `fk_fields_exercises_idx` (`exercises_id`),
   CONSTRAINT `fk_fields_exercises` FOREIGN KEY (`exercises_id`) REFERENCES `exercises` (`id`) ON DELETE CASCADE ON UPDATE NO ACTION
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb3;
 
 -- Les données exportées n'étaient pas sélectionnées.
 
+/*!40103 SET TIME_ZONE=IFNULL(@OLD_TIME_ZONE, 'system') */;
 /*!40101 SET SQL_MODE=IFNULL(@OLD_SQL_MODE, '') */;
 /*!40014 SET FOREIGN_KEY_CHECKS=IFNULL(@OLD_FOREIGN_KEY_CHECKS, 1) */;
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
